e827a71933bd1a0d0cff90922341933044122d4d
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-14 18:12:47 -0300

    [refactor] adiciona variavel de ambiente ao conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  user: 'root',
-  password: 'password',
-  database: 'TalkerDB',
-  port: 3306,
-  host: 'db',
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PORT || 'password',
+  database: process.env.MYSQL_DATABASE || 'TalkerDB',
+  host: process.env.MYSQL_HOSTNAME || 'db',
 });
 
 module.exports = connection;
