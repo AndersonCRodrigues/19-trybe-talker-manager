c7068e26c3fdb99abd6f2d5a99f7162a628f4134
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-14 14:42:45 -0300

    [feat] add rota GET/talker/:id

--- src/controllers/talkerController.js
+++ src/controllers/talkerController.js
@@ -12,4 +12,18 @@ module.exports = class TalkerController {
       res.status(500).json({ message: `Error: ${e}` });
     }
   }
+
+  static async talkerId(req, res) {
+    const { id } = req.params;
+    try {
+      const talker = await readTalkers();
+      const data = talker.find((talk) => talk.id === +id);
+      if (data) {
+        return res.status(200).json(data);
+      }
+      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    } catch (e) {
+      res.status(500).json({ message: `Error: ${e}` });
+    }
+  }
 };
--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -3,6 +3,6 @@ const TalkerController = require('../controllers/talkerController');
 
 const router = Router();
 
-router.get('/', TalkerController.allTalks);
-
+router.get('/', TalkerController.allTalks)
+  .get('/:id', TalkerController.talkerId);
 module.exports = router;
